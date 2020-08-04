761770a43dc26036876126316409d8f7fd44c82d
Graham Billington <grahamhbillington@gmail.com>
2020-08-04 13:00:16 -0400

    intitial commit

---
+++ js/sketch.js
@@ -0,0 +1,26 @@
+// globals
+var canvas;
+var canvasHeight = window.innerHeight;
+var canvasWidth = window.innerWidth;
+
+
+// setup function (called ONCE when the page first loads)
+function setup() {
+    // create canvas and append it to the page
+    canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent('parent');
+}
+
+// called when the window is resized
+function windowResized() {
+    // redefine the width and heights
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+}
+// draw function (called 60 times per second)
+function draw() {
+    // resized canvas
+    resizeCanvas(canvasWidth, canvasHeight)
+
+    background(25)
+}
